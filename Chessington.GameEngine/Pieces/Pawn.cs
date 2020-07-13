4eb687410b46e9df4c2a1805e3f835bc1fc7f9a3
Ruby Lavender <ruby.lavender@softwire.com>
2020-07-13 16:59:34 +0100

    Red 17, pawns can DESTROY enemeies

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -24,15 +24,20 @@ namespace Chessington.GameEngine.Pieces
             var pawnMoves = new List<Square>();
             Square forwards = new Square();
             Square twoForwards = new Square();
+            List<Square> diagonals = new List<Square>();
             if (this.Player == Player.White)
             {
                 forwards = Square.At(currentSquare.Row - 1, currentSquare.Col);
                 twoForwards = Square.At(currentSquare.Row - 2, currentSquare.Col);
+                diagonals.Add(Square.At(currentSquare.Row - 1, currentSquare.Col-1));
+                diagonals.Add(Square.At(currentSquare.Row - 1, currentSquare.Col+1));
             }
             else if(this.Player == Player.Black)
             {
                 forwards = Square.At(currentSquare.Row + 1, currentSquare.Col);
                 twoForwards = Square.At(currentSquare.Row + 2, currentSquare.Col);
+                diagonals.Add(Square.At(currentSquare.Row + 1, currentSquare.Col-1));
+                diagonals.Add(Square.At(currentSquare.Row + 1, currentSquare.Col+1));
             }
 
             if (forwards.IsOnTheBoard())
@@ -43,6 +48,13 @@ namespace Chessington.GameEngine.Pieces
                         if(board.GetPiece(twoForwards) == null)
                             pawnMoves.Add(twoForwards);
                 }
+
+            foreach (var diagonal in diagonals)
+            {
+                if(diagonal.IsOnTheBoard())
+                    if(board.GetPiece(diagonal)!=null && board.GetPiece(diagonal).Player!=this.Player)
+                        pawnMoves.Add(diagonal);
+            }
             return pawnMoves;
             
         }
