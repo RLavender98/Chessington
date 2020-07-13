bb60089268b5ee29f4242aa2b84646713cf56298
Ruby Lavender <ruby.lavender@softwire.com>
2020-07-13 13:58:19 +0100

    King moves red 07

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,17 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var availableMoves = new List<Square>();
+            Square currentSquare = board.FindPiece(this);
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j ++)
+                {    
+                    availableMoves.Add(Square.At(currentSquare.Row+i,currentSquare.Col+j));
+                }
+            }
+            availableMoves.Remove(currentSquare);
+            return availableMoves;
         }
     }
 }
