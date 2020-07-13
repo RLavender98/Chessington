b2795de604552d144ab4fe75f30de2d95e06eb28
Ruby Lavender <ruby.lavender@softwire.com>
2020-07-13 11:08:03 +0100

    passes Red 01

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.CodeDom;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +11,25 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var availableMoves = new List<Square>();
+            Square currentSquare = board.FindPiece(this);
+            if (this.Player == Player.White)
+            {
+                Square forwards = Square.At(currentSquare.Row - 1, currentSquare.Col);
+                if (board.GetPiece(forwards) == null)
+                {
+                    availableMoves.Add(forwards);
+                }
+            }
+            else if(this.Player == Player.White)
+            {
+                Square forwards = Square.At(currentSquare.Row + 1, currentSquare.Col);
+                if (board.GetPiece(forwards) == null)
+                {
+                    availableMoves.Add(forwards);
+                }
+            }
+            return availableMoves;
         }
     }
 }
