89e04700f0cf5f334a3020fc16b7edeb15ec5cd3
Ruby Lavender <ruby.lavender@softwire.com>
2020-07-13 13:44:56 +0100

    queen can move red 05

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +11,31 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var availableMoves = new List<Square>();
+            Square currentSquare = board.FindPiece(this);
+            for (int col = 0; col < 8; col++)
+            {
+                availableMoves.Add(Square.At(currentSquare.Row,col));
+            }
+            for (int row = 0; row < 8; row++)
+            {
+                availableMoves.Add(Square.At(row,currentSquare.Col));
+            }
+            for (int i = Math.Max(0 - currentSquare.Row, 0 - currentSquare.Col);
+                i < 8 - currentSquare.Col && i < 8 - currentSquare.Row;
+                i++)
+            {
+                availableMoves.Add(Square.At(currentSquare.Row+i,currentSquare.Col+i));
+            }
+            for (int i = Math.Max(currentSquare.Row - 7, 0-currentSquare.Col); 
+                i < 8 - currentSquare.Col && i < currentSquare.Row; 
+                i++)
+            {
+                availableMoves.Add(Square.At(currentSquare.Row -i,currentSquare.Col +i));
+            }
+
+            availableMoves.RemoveAll(s=>s==currentSquare);
+            return availableMoves;
         }
     }
 }
