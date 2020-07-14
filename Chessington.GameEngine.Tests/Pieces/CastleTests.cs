dbc5fc584654e4d161f90cc6af3b19e0e5e38bd3
Ruby Lavender <ruby.lavender@softwire.com>
2020-07-14 19:49:01 +0100

    Added empty castle tests to be filled in

---
+++ Chessington.GameEngine.Tests/Pieces/CastleTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace Chessington.GameEngine.Tests.Pieces
+{
+    [TestFixture]
+    public class CastleTests
+    {
+        [Test]
+        public void Castling_OnTheLeft_IsAnAvailableKingMove()
+        {
+            
+        }
+
+        [Test]
+        public void Castling_OnTheRight_IsAnAvailableKingMove()
+        {
+            
+        }
+
+        [Test]
+        public void CannotCastle_Through_A_Piece()
+        {
+            
+        }
+
+        [Test]
+        public void CannotCastle_Through_Check()
+        {
+            
+        }
+
+        [Test]
+        public void CannotCastle_If_KingHasAlreadyMoved()
+        {
+            
+        }
+        
+        [Test]
+        public void CannotCastle_If_RookHasAlreadyMoved()
+        {
+            
+        }
+
+        [Test]
+        public void RookJumpsOver_WhenCastlingOnTheLeft()
+        {
+            
+        }
+
+        [Test]
+        public void RookJumpsOver_WhenCastlingOnTheRight()
+        {
+            
+        }
+    }
+}
